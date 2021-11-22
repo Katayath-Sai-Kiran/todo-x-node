93ecb3b3aa4cd341b5d259498253b9ce3d440959
k Sai Kiran <ksaikiran0407@gmail.com>
2021-11-22 13:26:15 +0530

    added const colors

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xFF7966e4);
+const Color kPrimaryDarkColor = Color(0xFF36338c);
+const Color kBackgroundColor = Color(0xFFFFFFFF);
+const Color kTexthihglightColor = Color(0xFF0656ac);
+const Color kLinkAcentColor = Color(0xFFFFEFE7);
+const Color kGreenAcentColor = Color(0xFFeff9f8);
+const Color kBlueAcentColor = Color(0xFFe7e5f2);
+
+
+
